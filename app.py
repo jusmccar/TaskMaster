07f9639d11fc6c3de910ab36256b63ee689650f6
jusmccar <justinmccartney1@yahoo.com>
2022-07-09 20:44:35 -0700

    Add database

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from os import path
+
+DB_NAME = "database.db"
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DB_NAME
+db = SQLAlchemy(app)
+
+if not path.exists(DB_NAME):
+    db.create_all(app=app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Task %r>" % self.id
 
 @app.route("/")
 def index():
