faf8239b0b648fceaa6a2dc10af00039c8168191
jusmccar <justinmccartney1@yahoo.com>
2022-07-09 21:55:19 -0700

    Update and delete task functionality

--- app.py
+++ app.py
@@ -33,10 +33,36 @@ def index():
             db.session.commit()
             return redirect(url_for(".index"))
         except:
-            return "There was an issue adding your task."
+            return "There was an issue adding that task."
     else:
         tasks = Task.query.order_by(Task.date_created).all()
         return render_template("index.html", tasks=tasks)
+
+@app.route("/update/<int:id>", methods=["POST", "GET"])
+def update(id):
+    task = Task.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form["content"]
+
+        try:
+            db.session.commit()
+            return redirect(url_for(".index"))
+        except:
+            return "There was an issue updating that task."
+    else:
+        return render_template("update.html", task=task)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task = Task.query.get_or_404(id)
+
+    try:
+        db.session.delete(task)
+        db.session.commit()
+        return redirect(url_for(".index"))
+    except:
+        return "There was an issue deleting that task."
 
 if __name__ == "__main__":
     app.run(debug=True)
